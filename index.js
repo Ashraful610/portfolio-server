b51806e1877bf4a8b8d20e8ec8c8d476e6e8574d
Ashraful610 <ashalam610@gmail.com>
2022-06-14 11:25:17 +0600

    'update'

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(cors())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.adiia.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 async function run () {
     try{
         await client.connect()
@@ -169,7 +170,7 @@ run().catch(console.dir)
 
 
 app.get('/', (req, res) => {
-    res.send(' my portfolio website is here')
+    res.send(' my portfolio website working')
   })
   
   app.listen(port, () => {
